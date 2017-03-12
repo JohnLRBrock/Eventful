9147f1f9ef97d70c8a82a3b7e3c0d42e5389343e
khanthulhu <johnlrbrock@gmail.com>
2017-03-12 10:17:50 -0400

    altered migrations

--- db/migrate/20170304171722_create_events.rb
+++ db/migrate/20170304171722_create_events.rb
@@ -3,8 +3,8 @@ class CreateEvents < ActiveRecord::Migration[5.0]
     create_table :events do |t|
       t.string :name
       t.string :location
-      t.date :date
-      t.time :time
+      t.text :description
+      t.datetime :datetime
       t.integer :creator_id
 
       t.timestamps
--- db/migrate/20170306170436_add_description_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToEvents < ActiveRecord::Migration[5.0]
-  def change
-    add_column :events, :description, :text
-  end
-end
