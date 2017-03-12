86ff966dd024eedfe621db774d58dda97378dcf4
khanthulhu <johnlrbrock@gmail.com>
2017-03-12 09:43:15 -0400

    removed unnecessary migration

--- db/migrate/20170304191139_add_foreign_keys_to_events.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeysToEvents < ActiveRecord::Migration[5.0]
-  def change
-    add_foreign_key :events, :host_id
-    add_foreign_key :events, :guest_id
-  end
-end
--- db/migrate/20170305131011_remove_foreign_keys_host_and_guest.rb
+++
@@ -1,6 +0,0 @@
-class RemoveForeignKeysHostAndGuest < ActiveRecord::Migration[5.0]
-  def change
-    remove_foreign_key :events, :host_id
-    remove_foreign_key :events, :guest_id
-  end
-end
